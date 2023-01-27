779c5a0810b2849fd9a2c7256754e95a8d5ce1b6
Joel Nitta <joelnitta@gmail.com>
2023-01-27 14:56:46 +0900

    Initial commit

---
+++ family_tree.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+library(ftolr)
+library(ape)
+library(MonoPhy)
+library(assertr)
+
+# Load ultrametric fern tree, drop outgroup
+phy <- ft_tree(branch_len = "ultra", rooted = TRUE, drop_og = TRUE)
+
+# Load fern taxonomy
+taxonomy <- ftol_taxonomy %>%
+  # Fix one incorrect family: Dryopolystichum should be in Lomariopsidaceae
+  mutate(
+      family = case_when(
+        genus == "Dryopolystichum" ~ "Lomariopsidaceae",
+        TRUE ~ family
+      )
+    ) %>%
+  # Subset to only species in tree
+  filter(species %in% phy$tip.label)
+
+# Analyze monophyly of each famiy
+family_mono_test <- AssessMonophyly(
+  phy,
+  as.data.frame(taxonomy[, c("species", "family")])
+)
+
+# Check that all families are monophyletic or monotypic
+family_mono_summary <-
+  family_mono_test$family$result %>%
+  rownames_to_column("family") %>%
+  as_tibble() %>%
+  assert(in_set("Yes", "Monotypic"), Monophyly)
+
+# Get one exemplar tip (species) per family
+rep_tips <-
+  taxonomy %>%
+  group_by(family) %>%
+  slice(1) %>%
+  ungroup()
+
+# Subset phylogeny to one tip per family
+phy_family <- ape::keep.tip(phy, rep_tips$species)
+
+# Relabel with family names
+new_tips <-
+tibble(species = phy_family$tip.label) %>%
+  left_join(rep_tips, by = "species") %>%
+  pull(family)
+
+phy_family$tip.label <- new_tips
+
+# Visualize tree
+plot(ladderize(phy_family))
+
+# Write out tree
+ape::write.tree(phy_family, paste0("ftol_family_tree_v", ft_data_ver(), ".tre"))
